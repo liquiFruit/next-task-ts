42faa3d9121bedaec7293d626aaeddf884f16d78
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-04-24 23:06:18 +0200

    basic task page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "muted-dark": "#102A29",
+        "muted-light": "#103937",
+        "primary": "#00804B",
+        "secondary": "#ADD198",
+        "white": "#EAFAFA"
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
